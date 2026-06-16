b609df6598711fa6106dbed28e0378a9cb4a9c5f
Xnaizer <seykig028@gmail.com>
2026-06-17 01:05:04 +0700

    (initiate: blockchain): creating environtment with hardhat3 configuration using polygon amoy and alchemy to get data

---
+++ packages/blockchain/hardhat.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "hardhat/config";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthers],
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      type: "edr-simulated", 
+    },
+    polygon_amoy: {
+      type: "http",
+      url: process.env.ALCHEMY_AMOY_RPC_URL || "",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+      chainId: 80002,
+    },
+  },
+  paths: {
+    sources:   "./contracts",
+    tests:     "./test",
+    cache:     "./cache",
+    artifacts: "./artifacts",
+  },
+});
