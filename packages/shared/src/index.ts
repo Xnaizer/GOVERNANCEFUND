bf67fba5d28d01188d80da7e4c2b0d9f96137e67
Xnaizer <seykig028@gmail.com>
2026-06-25 02:06:03 +0700

    (feat: index): exporting data so other repo could use shared constant and abi's to communicate

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+export * from "./constants/eip712";
+export * from "./constants/contract";
+
+import Web3GovernanceABI from "./abi/Web3Governance.json";
+import RupiahTokenABI from "./abi/RupiahToken.json";
+import TrustedGatewayBurnerABI from "./abi/TrustedGatewayBurner.json";
+
+export { Web3GovernanceABI, RupiahTokenABI, TrustedGatewayBurnerABI };
