0bf8faa6cdabf879e589430ddf05b3fc8572a7e5
Xnaizer <seykig028@gmail.com>
2026-06-25 15:47:18 +0700

    (feat: contract): implement contract to shared data folder

--- packages/shared/src/constants/contract.ts
+++ packages/shared/src/constants/contract.ts
@@ -1,5 +1,6 @@
-
-RupiahToken                 :  0x386d5830aBA9f57e665ac9BC6E0200C5E1E4fF81
-TrustedGatewayBurner        :  0xfB08e2f8B039C76D07Df6F50F94460b177187961
-Web3Governance              :  0x24A7e58b751e42997c6f5f11165bD7FDcb3a9d80
-Deployer Address            :  0x3ff8C245C6499062A98Ed214EA606Ec5fa2627Dd
+export const CONTRACT_ADDRESS = {
+    rupiahToken: "0x386d5830aBA9f57e665ac9BC6E0200C5E1E4fF81",
+    trustedGatewayBurner: "0xfB08e2f8B039C76D07Df6F50F94460b177187961",
+    web3Governance: "0x24A7e58b751e42997c6f5f11165bD7FDcb3a9d80",
+    deployerAddress: "0x3ff8C245C6499062A98Ed214EA606Ec5fa2627Dd"
+} as const;
