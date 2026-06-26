51b36cc1ce737272784a6d687092b41e2be7fb4c
Xnaizer <seykig028@gmail.com>
2026-06-26 23:07:22 +0700

    (feat: contract): implement update contract due smart contract update

--- packages/shared/src/constants/contract.ts
+++ packages/shared/src/constants/contract.ts
@@ -1,6 +1,6 @@
 export const CONTRACT_ADDRESS = {
-    rupiahToken: "0x386d5830aBA9f57e665ac9BC6E0200C5E1E4fF81",
-    trustedGatewayBurner: "0xfB08e2f8B039C76D07Df6F50F94460b177187961",
-    web3Governance: "0x24A7e58b751e42997c6f5f11165bD7FDcb3a9d80",
+    rupiahToken: "0xe38D6D441fa654e7d8FC0D8d0061a5AB3D45A922",
+    trustedGatewayBurner: "0x84b72A968FAed6F29A1447b464aAC18063C2736f",
+    web3Governance: "0x7024aCA78122CdA4aDbB79b5815C757e83a2a1Da",
     deployerAddress: "0x3ff8C245C6499062A98Ed214EA606Ec5fa2627Dd"
 } as const;
