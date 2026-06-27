5bb01b740424a2fe4d2e23840b54ed20a20a298e
Xnaizer <seykig028@gmail.com>
2026-06-27 19:51:13 +0700

    (feat: redis): implement ioredis connection with upstash redis

---
+++ apps/api/src/lib/redis.ts
@@ -0,0 +1,14 @@
+import Redis from "ioredis";
+import { env } from "../config/env";
+
+export const redis = new Redis(env.UPSTASH_REDIS_URL, {
+    maxRetriesPerRequest: null
+});
+
+redis.on("connect", () => {
+    console.log("[SERVER] Connected to Upstash Redis");
+});
+
+redis.on("error", (err) => {
+    console.error("[SERVER] Upstash Connection error: ", err.message);
+});
