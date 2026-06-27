1bba8a50a23384cc237b8fbedb9034d0ed55a56b
Xnaizer <seykig028@gmail.com>
2026-06-28 00:45:04 +0700

    (feat: mailer): implemeting nodemailer transporter, render template html, sendtemplate email, verify mailer for smtp feature

---
+++ apps/api/src/lib/mailer.ts
@@ -0,0 +1,44 @@
+import nodemailer from "nodemailer";
+import { env } from "../config/env";
+import path from "node:path";
+import ejs from 'ejs';
+
+interface IEmail {
+    to: string;
+    subject: string;
+    template: string;
+    data: Record<string, unknown>;
+}
+
+const transporter = nodemailer.createTransport({
+    host: env.SMTP_HOST,
+    port: env.SMTP_PORT,
+    auth: {
+        user: env.SMTP_USER,
+        pass: env.SMTP_PASS
+    },
+});
+
+async function renderTemplate(
+    templateName: string,
+    data: Record<string, unknown>
+): Promise<string> {
+    const templatePath = path.join(__dirname, "..", "templates", "emails", `${templateName}.ejs`);
+    return ejs.renderFile(templatePath, data);
+}
+
+export async function sendTemplateEmail(params: IEmail ): Promise<void> {
+    const html = await renderTemplate(params.template, params.data);
+
+    await transporter.sendMail({
+        from: env.EMAIL_FROM,
+        to: params.to,
+        subject: params.subject,
+        html
+    });
+}
+
+export async function verifyMailer(): Promise<void> {
+    await transporter.verify();
+    console.log("[MAILER] SMTP Connection verified");
+}
