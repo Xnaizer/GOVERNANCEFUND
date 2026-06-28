12d8108b78d05f1ebb35b94e09abbad85186205d
Xnaizer <seykig028@gmail.com>
2026-06-28 12:53:17 +0700

    (feat: app): implementing api route v1 to app and use the router index

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import response from "./utils/response";
 import { notFoundHandler } from "./middleware/notFound";
 import { errorHandler } from "./middleware/errorHandler";
+import apiRouter from "./routes";
 
 const app: Express = express();
 
@@ -14,6 +15,7 @@ app.use(cors()); // cross origin
 app.use(morgan("dev")); // log every request
 
 app.use(express.json());
+app.use("/api/v1", apiRouter);
 
 app.get("/health", (_req: Request, res: Response): void => {
     response.success(res, "ok");
