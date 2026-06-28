99970d5432aa14499def838a8a766a57f8474063
Xnaizer <seykig028@gmail.com>
2026-06-28 12:50:33 +0700

    (feat: login route): adding login route to auth route

--- apps/api/src/routes/auth.ts
+++ apps/api/src/routes/auth.ts
@@ -1,10 +1,17 @@
 import express, {type Router} from "express";
 import authController from "../controllers/authController";
 import { asyncHandler } from "../utils/asyncHandler";
+import { authMiddleware } from "../middleware/auth";
 
 const router: Router  = express.Router();
 
 router.post("/register", asyncHandler(authController.register));
-router.get('/verify-email', asyncHandler(authController.verifyEmail));
+router.get("/verify-email", asyncHandler(authController.verifyEmail));
+router.post("/login", asyncHandler(authController.login));
+router.post(
+    "/logout", 
+    asyncHandler(authMiddleware), 
+    asyncHandler(authController.logout)
+);
 
 export default router;
