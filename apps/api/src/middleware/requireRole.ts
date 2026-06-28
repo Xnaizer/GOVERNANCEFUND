8d7d334734577ec5a9e5677c2ae30ba0b15df621
Xnaizer <seykig028@gmail.com>
2026-06-28 15:22:23 +0700

    (feat: role type): adding role type to acl middleware

--- apps/api/src/middleware/requireRole.ts
+++ apps/api/src/middleware/requireRole.ts
@@ -1,11 +1,12 @@
 import type { Request, Response, NextFunction } from "express";
 import { AppError } from "../utils/AppError";
+import { Role } from "@repo/database";
 
-export function requireRole(roles: string[]) {
+export function requireRole(roles: Role[]) {
     return (req: Request, _res: Response, next: NextFunction): void => {
         const role = req.user?.role;
 
-        if(!role || !roles.includes(role)) {
+        if(!role || !roles.includes(role as Role)) {
             throw new AppError("Forbidden: insufficient role", 403);
         }
 
