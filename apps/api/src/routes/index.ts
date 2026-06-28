cbb0555efb3e1e87d7c454519b3923e6f4266316
Xnaizer <seykig028@gmail.com>
2026-06-28 21:45:38 +0700

    (feat: index route): implementing index route

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,8 +1,10 @@
-import express, {type Router} from "express";
-import authRouter from "./auth";
+import express, { type Router } from "express";
+import authRouter from "./auths";
+import userRouter from "./users";
 
 const router: Router  = express.Router();
 
 router.use("/auth", authRouter);
+router.use("/users", userRouter);
 
 export default router;
