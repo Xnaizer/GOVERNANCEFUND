50f3d49966e799ea98e89b9717eb1b301ed4686a
Xnaizer <seykig028@gmail.com>
2026-06-29 11:19:45 +0700

    (feat: viem): implementing public client to read data on chain

---
+++ apps/api/src/lib/viem.ts
@@ -0,0 +1,8 @@
+import { createPublicClient, http, type PublicClient } from "viem";
+import { baseSepolia } from "viem/chains";
+import { env } from "../config/env";
+
+export const publicClient: PublicClient<ReturnType<typeof http>, typeof baseSepolia> = createPublicClient({
+    chain: baseSepolia,
+    transport: http(env.ALCHEMY_BASE_SEPOLIA_RPC_URL)
+});
