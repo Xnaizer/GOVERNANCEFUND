5e5bef6fcf00ac03c1f98409beaa961cf2c3f465
Xnaizer <seykig028@gmail.com>
2026-06-29 19:23:18 +0700

    (feat: route public): adding route for public use

---
+++ apps/api/src/routes/public.ts
@@ -0,0 +1,26 @@
+import express, { type Router } from "express";
+import publicController from "../controllers/publicController";
+import { readLimiter } from "../middleware/rateLimiter";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const router: Router = express.Router();
+
+router.get(
+    "/programs",
+    readLimiter,
+    asyncHandler(publicController.listPrograms)
+);
+
+router.get(
+    "/programs/:id",
+    readLimiter,
+    asyncHandler(publicController.detailProgram)
+);
+
+router.get(
+    "/stats",
+    readLimiter,
+    asyncHandler(publicController.stats)
+)
+
+export default router;
