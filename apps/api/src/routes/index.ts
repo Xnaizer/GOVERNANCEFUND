f03b93e75985a15f04dda86e0be653d752cc4c96
Xnaizer <seykig028@gmail.com>
2026-06-29 19:23:47 +0700

    (feat: index route): adding public to index route

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -2,11 +2,13 @@ import express, { type Router } from "express";
 import authRouter from "./auths";
 import userRouter from "./users";
 import programRouter from "./programs";
+import publicRouter from "./public";
 
 const router: Router  = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/programs", programRouter);
+router.use("/public", publicRouter);
 
 export default router;
