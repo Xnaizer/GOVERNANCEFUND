3de8790db43c448e4e922aa09912fc27f2bf9e6e
Xnaizer <seykig028@gmail.com>
2026-06-29 15:28:30 +0700

    (fix: export type): adding ABI export type to unknown to avoid error message

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,8 +2,10 @@ export * from "./constants/eip712";
 export * from "./constants/contract";
 export * from "./utils/programHash";
 
-import Web3GovernanceABI from "./abi/Web3Governance.json";
-import RupiahTokenABI from "./abi/RupiahToken.json";
-import TrustedGatewayBurnerABI from "./abi/TrustedGatewayBurner.json";
+import Web3GovernanceArtifact from "./abi/Web3Governance.json";
+import RupiahTokenArtifact from "./abi/RupiahToken.json";
+import TrustedGatewayBurnerArtifact from "./abi/TrustedGatewayBurner.json";
 
-export { Web3GovernanceABI, RupiahTokenABI, TrustedGatewayBurnerABI };
+export const Web3GovernanceABI = Web3GovernanceArtifact.abi as unknown as readonly unknown[];
+export const RupiahTokenABI = RupiahTokenArtifact.abi as unknown as readonly unknown[];
+export const TrustedGatewayBurnerABI = TrustedGatewayBurnerArtifact.abi as unknown as readonly unknown[];
