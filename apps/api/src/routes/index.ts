02390cbfe5a8536eda05d081450ad08eef12cf21
Xnaizer <seykig028@gmail.com>
2026-06-29 11:33:52 +0700

    (feat: index): implementing index route for program route

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express, { type Router } from "express";
 import authRouter from "./auths";
 import userRouter from "./users";
+import programRouter from "./programs";
 
 const router: Router  = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
+router.use("/programs", programRouter);
 
 export default router;
