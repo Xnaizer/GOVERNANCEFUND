1d51f775b4582d5fcb23d1e2de0d868bfc979b77
Xnaizer <seykig028@gmail.com>
2026-07-03 03:34:40 +0700

    (update: webhook route): implementig queue for every incoming event data by alchemy

--- apps/api/src/routes/webhook.ts
+++ apps/api/src/routes/webhook.ts
@@ -2,7 +2,7 @@ import express , { type Router } from "express";
 import { webhookVerify } from "../middleware/webhookVerify";
 import { asyncHandler } from "../utils/asyncHandler";
 import response from "../utils/response";
-import * as webhookService from "../services/webhookService";
+import { webhookIngestionQueue } from "../queues/queues";
 
 const router: Router = express.Router();
 
@@ -13,14 +13,14 @@ router.post(
         const payload = JSON.parse(req.body.toString("utf8"));
         
         if(payload.eventName) {
-            await webhookService.dispatchEvent({
+            await webhookIngestionQueue.add("event", {
                 eventName: payload.eventName,
                 args: payload.args ?? {},
-                txHash: payload.txHash
+                txHash: payload.txHash,
             });
         }
 
-        response.success(res, "Webhook received");
+        response.success(res, "Webhook queued");
     })
 )
 
