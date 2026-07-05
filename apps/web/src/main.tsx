4292252b09a2453a6b627040df1544abe1fd54cc
Xnaizer <seykig028@gmail.com>
2026-07-05 17:05:15 +0700

    (feat: main): implementing provider to app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@rainbow-me/rainbowkit/styles.css";
 import "./index.css";
+import { AppProviders } from "./providers/AppProviders";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </React.StrictMode>
 );
