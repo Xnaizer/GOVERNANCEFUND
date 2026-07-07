2d2dfe1199e6cdbef16ffc39b834b7a6438fa2bd
Xnaizer <seykig028@gmail.com>
2026-07-07 17:22:50 +0700

    (feat: error mid): adding instance of Multer error response

--- apps/api/src/middleware/errorHandler.ts
+++ apps/api/src/middleware/errorHandler.ts
@@ -3,6 +3,7 @@ import { AppError } from "../utils/AppError";
 import response from "../utils/response";
 import { env } from "../config/env";
 import { logger } from "../lib/logger";
+import { MulterError } from "multer";
 
 export function errorHandler(
     err: Error,
@@ -14,6 +15,13 @@ export function errorHandler(
         response.error(res, err.message, err.statusCode);
         return;
     }
+
+    if (err instanceof MulterError) {
+        const msg = err.code === "LIMIT_FILE_SIZE" ? "File too large (max exceeded)" : err.message;
+        response.error(res, msg, 400);
+        return;
+    }
+
 
     logger.error({ err }, "[UNHANDLED ERROR]");
 
