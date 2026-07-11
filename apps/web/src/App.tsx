355c4f169e8bd387a3ecf74ad02d164f3744a203
Xnaizer <seykig028@gmail.com>
2026-07-11 11:24:19 +0700

    feat(web): update apps/web/src/App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,23 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing } from "./pages/Landing";
-import { ExplorerPage } from "./pages/ExplorerPage";
-import { ProgramDetailPage } from "./pages/ProgramDetailPage";
-import { RegisterPage } from "./pages/RegisterPage";
-import { LoginPage } from "./pages/LoginPage";
-import { VerifyEmailPage } from "./pages/VerifyEmailPage";
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes/AppRoutes";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+import { OfflineBanner } from "./components/OfflineBanner";
+import { ScrollToTop } from "./components/ScrollToTop";
+import { RouteProgress } from "./components/RouteProgress";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/programs" element={<ExplorerPage />} />
-        <Route path="/programs/:id" element={<ProgramDetailPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/verify-email" element={<VerifyEmailPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <OfflineBanner />
+      <BrowserRouter>
+        <ScrollToTop />
+        <RouteProgress />
+        <AppRoutes />
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
