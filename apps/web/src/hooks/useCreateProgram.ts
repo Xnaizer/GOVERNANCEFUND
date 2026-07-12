419ae8a8579bc54cb1cf5d0f24ff88dbb88ffb2e
xnaizer <seykig028@gmail.com>
2026-07-12 22:46:54 +0700

    refactor(web): useCreateProgram.ts pakai services/

--- apps/web/src/hooks/useCreateProgram.ts
+++ apps/web/src/hooks/useCreateProgram.ts
@@ -1,6 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
-import { createProgram, type CreateProgramInput } from "../api/programApi";
+import { createProgram, type CreateProgramInput } from "../services/programApi";
 
 export function useCreateProgram() {
-  return useMutation({ mutationFn: (input: CreateProgramInput) => createProgram(input) });
+  return useMutation({
+    mutationFn: (input: CreateProgramInput) => createProgram(input),
+  });
 }
