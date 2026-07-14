2d0aac33db75e2fea88f9e0c563e3149bc756c53
Xnaizer <seykig028@gmail.com>
2026-07-14 18:44:39 +0700

    (alter: smtp): alter to gmail from mailtrap

--- apps/api/src/lib/mailer.ts
+++ apps/api/src/lib/mailer.ts
@@ -13,10 +13,13 @@ interface IEmail {
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
   port: env.SMTP_PORT,
+  secure: env.SMTP_PORT === 465, 
   auth: {
     user: env.SMTP_USER,
     pass: env.SMTP_PASS,
   },
+  connectionTimeout: 10_000, 
+  greetingTimeout: 10_000,
 });
 
 async function renderTemplate(
